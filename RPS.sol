710cd0c67a4c337b61901b73fba7edf047bf4eef
ProgrammerTon <pattarapol.nanetra@gmail.com>
2025-03-20 11:52:59 +0700

    TestCode

--- RPS.sol
+++ RPS.sol
@@ -1,58 +1,87 @@
-
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
 contract RPS {
     uint public numPlayer = 0;
     uint public reward = 0;
-    mapping (address => uint) public player_choice; // 0 - Rock, 1 - Paper , 2 - Scissors
-    mapping(address => bool) public player_not_played;
+    mapping(address => bytes32) public player_commit;
+    mapping(address => bool) public player_revealed;
     address[] public players;
-
-    uint public numInput = 0;
-
-    function addPlayer() public payable {
-        require(numPlayer < 2);
-        if (numPlayer > 0) {
-            require(msg.sender != players[0]);
-        }
-        require(msg.value == 0.000001 ether);
-        reward += msg.value;
-        player_not_played[msg.sender] = true;
-        players.push(msg.sender);
-        numPlayer++;
+    
+    uint public revealDeadline;
+    CommitReveal public commitReveal;
+    TimeUnit public timeUnit;
+    
+    constructor(address _commitReveal, address _timeUnit) {
+        commitReveal = CommitReveal(_commitReveal);
+        timeUnit = TimeUnit(_timeUnit);
     }
 
-    function input(uint choice) public  {
-        require(numPlayer == 2);
-        require(player_not_played[msg.sender]);
-        require(choice == 0 || choice == 1 || choice == 2);
-        player_choice[msg.sender] = choice;
-        player_not_played[msg.sender] = false;
-        numInput++;
-        if (numInput == 2) {
+    function addPlayer(bytes32 commitHash) public {
+        require(numPlayer < 2, "Game is full");
+        if (numPlayer > 0) {
+            require(msg.sender != players[0], "Player already registered");
+        }
+        player_commit[msg.sender] = commitHash;
+        players.push(msg.sender);
+        numPlayer++;
+        
+        if (numPlayer == 2) {
+            timeUnit.setStartTime();
+        }
+    }
+
+    function reveal(bytes32 choice) public {
+        require(numPlayer == 2, "Not enough players");
+        require(!player_revealed[msg.sender], "Already revealed");
+        require(commitReveal.getHash(choice) == player_commit[msg.sender], "Invalid reveal");
+        
+        player_revealed[msg.sender] = true;
+        commitReveal.reveal(choice);
+        
+        if (player_revealed[players[0]] && player_revealed[players[1]]) {
             _checkWinnerAndPay();
         }
     }
 
     function _checkWinnerAndPay() private {
-        uint p0Choice = player_choice[players[0]];
-        uint p1Choice = player_choice[players[1]];
+        uint p0Choice = uint(uint256(commitReveal.getHash(keccak256(abi.encodePacked(players[0])))) % 3);
+        uint p1Choice = uint(uint256(commitReveal.getHash(keccak256(abi.encodePacked(players[1])))) % 3);
         address payable account0 = payable(players[0]);
         address payable account1 = payable(players[1]);
+        
         if ((p0Choice + 1) % 3 == p1Choice) {
-            // to pay player[1]
             account1.transfer(reward);
-        }
-        else if ((p1Choice + 1) % 3 == p0Choice) {
-            // to pay player[0]
+        } else if ((p1Choice + 1) % 3 == p0Choice) {
             account0.transfer(reward);    
-        }
-        else {
-            // to split reward
+        } else {
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
         }
     }
+    
+    function claimTimeout() public {
+        require(timeUnit.elapsedSeconds() > 300, "Time not expired");
+        
+        if (player_revealed[players[0]] && !player_revealed[players[1]]) {
+            payable(players[0]).transfer(reward);
+        } else if (!player_revealed[players[0]] && player_revealed[players[1]]) {
+            payable(players[1]).transfer(reward);
+        } else {
+            payable(msg.sender).transfer(reward);
+        }
+    }
+}
+
+contract CommitReveal {
+    function getHash(bytes32 data) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(data));
+    }
+    function reveal(bytes32 revealHash) public {}
+}
+
+contract TimeUnit {
+    uint256 public startTime;
+    function setStartTime() public { startTime = block.timestamp; }
+    function elapsedSeconds() public view returns (uint256) { return (block.timestamp - startTime); }
 }
